41ce9b44f8e09f585533183dea81fe2155177cef
takion <jo@surikat.pro>
2018-12-29 11:39:57 +0100

    gh pages

---
+++ docs/precache-manifest.daad2a756795f4e38752a0a36a8e68f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b617b5e000ba12afa6a1",
+    "url": "/static/css/main.7512bc14.chunk.css"
+  },
+  {
+    "revision": "b617b5e000ba12afa6a1",
+    "url": "/static/js/main.b617b5e0.chunk.js"
+  },
+  {
+    "revision": "14c6129ea3dcf804198e",
+    "url": "/static/css/1.b803d528.chunk.css"
+  },
+  {
+    "revision": "14c6129ea3dcf804198e",
+    "url": "/static/js/1.14c6129e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c7df7809a5374dc24f899540aac20b9",
+    "url": "/static/media/kaya.4c7df780.jpg"
+  },
+  {
+    "revision": "2908aafa92e90cf53d930b501951418b",
+    "url": "/static/media/square-black.2908aafa.png"
+  },
+  {
+    "revision": "2364369e8ad234cc97b910cbd5f15a01",
+    "url": "/static/media/square-white.2364369e.png"
+  },
+  {
+    "revision": "72aa6bdcc55feff5febb75e1400d817f",
+    "url": "/index.html"
+  }
+];
